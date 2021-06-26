5ac0c2341d7db609c13fbcb8cf7da0678c25b5a1
Junior Macedo <80720405+JuniorMacedo91@users.noreply.github.com>
2021-06-26 19:24:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,33 @@
+
+var imgIcon = document.getElementById('updatePic');
+var image = document.getElementById('profilePic');
+var file = document.getElementById('file')
+var btnFile = document.getElementById('btn-file');
+
+
+//Show file button
+imgIcon.addEventListener('mouseenter', function () {
+    btnFile.style.display = 'inline'
+})
+
+//Hide file button
+imgIcon.addEventListener('mouseleave', function () {
+    btnFile.style.display = 'none'
+})
+
+//Upload photo
+file.addEventListener('change', function(){
+    let photo = this.files[0]
+
+    if(photo){
+         let showPic = new FileReader()
+
+         showPic.addEventListener('load', function(){
+            image.setAttribute('src', showPic.result)
+         })
+         
+         showPic.readAsDataURL(photo)
+    }
+})
+
+
